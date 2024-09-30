cbd250e366540eccb671b8a29c1141f7ab57372c
Kristian <kglomset@gmail.com>
2024-09-30 19:41:51 +0200

    workspaces setup

---
+++ web/variables.tf
@@ -0,0 +1,25 @@
+variable "rg_name" {
+    type = string
+    description = "Name of the resource group"
+}
+
+variable "location" {
+    type = string
+    description = "The location region of the resource group"
+}
+
+variable "sa_name" {
+    type = string
+    description = "Name of the storage account"
+}
+
+variable "index_document" {
+    type = string
+    description = "Name of the index document"
+}
+
+variable "source_content" {
+    type = string
+    description = "Source content for the index.html file"
+}
+
