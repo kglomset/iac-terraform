f122e03246cc0016f2a8826dea4568f236d561d0
Kristian <kglomset@gmail.com>
2024-10-03 13:11:20 +0200

    destroy

--- 39-static-website/main.tf
+++ 39-static-website/main.tf
@@ -22,7 +22,7 @@ resource "azurerm_storage_account" "sa_web" {
   resource_group_name      = azurerm_resource_group.rg_web.name
   location                 = azurerm_resource_group.rg_web.location
   account_tier             = "Standard"
-  account_replication_type = "LRS"
+  account_replication_type = "GRS"
 
   static_website {
     index_document = var.index_document
@@ -41,3 +41,5 @@ resource "azurerm_storage_blob" "index_html" {
 output "primary_web_endpoint" {
   value = azurerm_storage_account.sa_web.primary_web_endpoint
 }
+
+#Destroy test
